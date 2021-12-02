52b4587a8a5c7101e6fab0ad3e80f806d88893cc
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-12-02 17:14:37 +0500

    feat:users: implement db manipulating methods

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,21 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+let users = [];
+
+const getAll = async () => users;
+
+const getById = async (id) => users.find((el) => el.id === id);
+
+const create = async (user) => {
+  users.push(user);
+  return user;
 };
 
-module.exports = { getAll };
+const update = async (id, data) => {
+  const userIndex = users.findIndex((el) => el.id === id);
+  return Object.assign(users[userIndex], data);
+};
+
+const remove = async (id) => {
+  users = users.filter((el) => el.id !== id);
+};
+
+module.exports = { getAll, getById, create, update, remove };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,17 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model');
 
-const getAll = () => usersRepo.getAll();
+const getAll = () => Promise.resolve(usersRepo.getAll());
 
-module.exports = { getAll };
+const getById = async (id) => Promise.resolve(usersRepo.getById(id));
+
+const create = async (userData) => {
+  const user = new User({ ...userData });
+  return Promise.resolve(usersRepo.create(user));
+};
+
+const update = async (id, data) => Promise.resolve(usersRepo.update(id, data));
+
+const remove = async (id) => Promise.resolve(usersRepo.remove(id));
+
+module.exports = { getAll, getById, create, update, remove };
