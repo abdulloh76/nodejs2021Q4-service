cb4ef17196fc65a61c8bdb22db3d1cd66dda7c35
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-12-08 15:01:23 +0500

    refactor: resolve eslint warning

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,6 @@ import { PORT } from './common/config';
 import app from './app';
 
 app.listen(PORT, () =>
+  // eslint-disable-next-line no-console
   console.log(`App is running on http://localhost:${PORT}`)
 );
