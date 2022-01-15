577b0f8ec836adf2c7cee1acd3935a2a074012c0
Abdulloh Musayev <abdulloh_musayev@epam.com>
2022-01-15 18:42:20 +0500

    feat: install & connect - typeorm&postgres

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
+import { createConnection } from 'typeorm';
 import { PORT } from './common/config';
 import app from './app';
 
-app.listen(PORT, () =>
-  // eslint-disable-next-line no-console
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+createConnection({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [],
+  synchronize: true,
+  logging: false,
+})
+  .then(() => {
+    app.listen(PORT, () =>
+      // eslint-disable-next-line no-console
+      console.log(`App is running on http://localhost:${PORT}`)
+    );
+  })
+  .catch((error: Error) => {
+    process.stderr.write(`${error.name}`);
+    process.exit(1);
+  });
